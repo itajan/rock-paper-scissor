5c92db5618ff1e86f54ba329cf59ffa6e87dcd67
itajan <izaactajan@gmail.com>
2017-10-18 16:28:02 -0600

    Initial commit

---
+++ app/src/main/java/edu/cnm/deepdive/ca/rock_paper_scissor/activities/TerrainActivity.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.ca.rock_paper_scissor.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.deepdive.ca.rock_paper_scissor.R;
+
+public class TerrainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_terrain);
+  }
+}
