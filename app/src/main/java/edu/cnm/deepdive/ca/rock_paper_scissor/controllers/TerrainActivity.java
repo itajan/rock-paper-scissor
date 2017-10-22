12a9fe0e10acd65d913853e83cf4b5fcbd9ca3a6
itajan <izaactajan@gmail.com>
2017-10-22 11:08:09 -0600

    Added javadocs to TerrainActivity and created controllers/package-info

--- app/src/main/java/edu/cnm/deepdive/ca/rock_paper_scissor/controllers/TerrainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/ca/rock_paper_scissor/controllers/TerrainActivity.java
@@ -9,8 +9,11 @@ import edu.cnm.deepdive.ca.rock_paper_scissor.R;
 import edu.cnm.deepdive.ca.rock_paper_scissor.models.Terrain;
 import edu.cnm.deepdive.ca.rock_paper_scissor.views.TerrainView;
 
+/**
+ * Creates activity class for Rock/Paper/Scissors, cellular automata.
+ */
 public class TerrainActivity extends AppCompatActivity {
-
+/** Rest duration of overridden run method thread class inside nested activity class.*/
   private static final int RUNNER_THREAD_REST = 40;
   private static final int RUNNER_THREAD_SLEEP = 50;
 
@@ -78,29 +81,57 @@ public class TerrainActivity extends AppCompatActivity {
     return super.onCreateOptionsMenu(menu);
 
   }
-
+/** Instantiates a new terrain with a reference name terrain.
+ * Calls the initialize() method inherited from Terrain model to create a lattice with
+ * randomly assigned instance of Breed.
+ */
   private void initializeModel() {
     terrain = new Terrain();
     terrain.initialize();
   }
-
+/** Assign values for terrainView and terrainLayout objects.*/
   private void initializeUserInterface() {
     terrainLayout = findViewById(R.id.terrainLayout);
     terrainView = (TerrainView) findViewById(R.id.terrainView);
   }
 
+  /**
+   * Returns the currently specified value of running field.
+   * @return Return on or off flag
+   */
   private synchronized boolean isRunning() {
     return running;
   }
 
+  /**
+   * Sets the value of running to be used by the Runner class to determine whether to run
+   * terrain.step() and terrainView.setSource() methods.
+   * Allows the View to be updated or not based on the two conditions specified in
+   * the while loop inside the Runner class.
+   *
+   * @param running on or off flag
+   */
   private synchronized void setRunning(boolean running) {
     this.running = running;
   }
 
+  /**
+   *
+   * Returns the currently specified value of inForeground field based whether the app is paused,
+   * running or resetting.
+   *
+   * @return on or off flag.
+   */
   private synchronized boolean isInForeground() {
     return inForeground;
   }
 
+  /**
+   * Sets the value of inForeground to be used as a flag to determine whether to reset, pause, or
+   * run the app.
+   *
+   * @param inForeground on or off flag.
+   */
   private synchronized void setInForeground(boolean inForeground) {
     if (inForeground) {
       this.inForeground = true;
@@ -120,6 +151,12 @@ public class TerrainActivity extends AppCompatActivity {
     }
   }
 
+  /**
+   * Determines the state of Runner Thread and sets the Thread sleep method to rest or sleep based
+   * upon condition.
+   * Based upon the isRunning && isInForeground condition.
+   *
+   */
   private class Runner extends Thread {
 
     @Override
---
+++ app/src/main/java/edu/cnm/deepdive/ca/rock_paper_scissor/controllers/package-info.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * Classes that implement a Rock-Paper-Scissor (RPS) controller of an ecosystem with cyclic dominance between
+ * 3 species. This control methodology allows for the smooth communication and coordination
+ * between the view and the model structure of this app.
+ *
+ *
+ *
+ *@author Izaac Tajan
+ *@version 1.0, 2017-10-22
+ */
+package edu.cnm.deepdive.ca.rock_paper_scissor.controllers;
